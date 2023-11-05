898de0c8ee7e926ff8a9164a1d6862b22513b35d
LysandreLT <125960659+LysandreLT@users.noreply.github.com>
2023-11-05 21:01:13 +0100

    random word

--- Wordot.py
+++ Wordot.py
@@ -1,6 +1,21 @@
 from gensim.models import KeyedVectors
+import random
 
 model = KeyedVectors.load_word2vec_format("frWac_postag_no_phrase_700_skip_cut50.bin",
                                           binary=True, unicode_errors="ignore")
 
-print(model.most_similar("voiture_n"))
+# Choosing a random word
+word = random.choice(model.index_to_key)
+
+# Getting the most similar word of the random one
+mostSimilar, similarityOfMostSimilar = model.most_similar(word, topn=1)[0]
+print(word)
+print(mostSimilar)
+print(similarityOfMostSimilar)
+
+# Start of the game
+print("Choose a word :")
+attempt = input()
+if attempt == word:
+    print("vous avez gagné")
+
